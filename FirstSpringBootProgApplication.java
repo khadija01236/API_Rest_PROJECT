fd50e009e21f6445d22d6a34e3019e10c30b95e8
khadija01236 <146732164+khadija01236@users.noreply.github.com>
2024-09-29 16:23:42 +0200

    Add files via upload

---
+++ FirstSpringBootProgApplication.java
@@ -0,0 +1,13 @@
+package com.khadijaFirstProg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSpringBootProgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSpringBootProgApplication.class, args);
+	}
+
+}
